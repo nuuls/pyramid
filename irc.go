32d296c710052eff7ec66ca4b8f7e4800a19f0f7
nuuls <nilsv21@gmail.com>
2016-09-25 17:02:35 +0200

    first commit

---
+++ irc.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"net/textproto"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var (
+	conn net.Conn
+	re   = regexp.MustCompile(`:(\w+)!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #(\w+) :(.*)`)
+)
+
+var (
+	messages     int
+	messageLimit int = 18
+	mod          bool
+)
+
+func connect() {
+	var err error
+	if cfg.Host != "" {
+		conn, err = net.Dial("tcp", cfg.Host)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("connected to", cfg.Host)
+	} else {
+		conn, err = tls.Dial("tcp", "irc.chat.twitch.tv:443", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("connected to irc.chat.twitch.tv:443 using tls")
+	}
+
+	send("PASS " + cfg.Pass)
+	send("NICK " + cfg.Nick)
+	send("CAP REQ twitch.tv/tags")
+	send("CAP REQ twitch.tv/commands")
+	send("JOIN #" + channel)
+}
+
+func send(msg string) {
+	_, err := conn.Write([]byte(msg + "\r\n"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func say(msg string) {
+	for messages > messageLimit {
+		time.Sleep(time.Second * 1)
+		fmt.Println("waiting so you dont get global banned LUL")
+	}
+	fmt.Println("sent:", msg)
+	send("PRIVMSG #" + channel + " :" + msg + " ")
+	messages++
+	time.AfterFunc(time.Second*35, func() { messages-- })
+}
+
+func readChat() {
+	reader := bufio.NewReader(conn)
+	tp := textproto.NewReader(reader)
+	for {
+		line, err := tp.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if line == "PING " {
+			send(strings.Replace(line, "PING", "PONG", 1))
+		}
+
+		matches := re.FindStringSubmatch(line)
+		if len(matches) == 4 {
+			user, _, msg := matches[1], matches[2], matches[3]
+			fmt.Printf("%s: %s\n", user, msg)
+		} else {
+			if line != "" && !strings.HasPrefix(line, "@") {
+				fmt.Println(line)
+			}
+			spl := strings.Split(line, " ")
+			if strings.Contains(spl[0], "mod=1") && messageLimit != 95 {
+				messageLimit = 95
+				mod = true
+				fmt.Println("mod detected, message limit now set to", messageLimit, " / 30 seconds")
+			}
+			if strings.Contains(spl[0], "mod=0") && messageLimit != 18 {
+				messageLimit = 18
+				mod = false
+				fmt.Println("mod not detected, message limit now set to", messageLimit, " / 30 seconds")
+			}
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type config struct {
+	Host  string `json:"host"`
+	Nick  string `json:"nick"`
+	Pass  string `json:"pass"`
+	Sleep int    `json:"sleep"`
+}
+
+var sleep time.Duration
+var cfg config
+var channel string
+
+func init() {
+	bs, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(bs, &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sleep = time.Duration(cfg.Sleep) * time.Millisecond
+}
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("usage: pyramid.exe yourChannel")
+		fmt.Println("example: pyramid.exe nuuls")
+		os.Exit(1)
+	}
+	fmt.Print("usage: ")
+	fmt.Println("make a pyramid: Kappa 5")
+	fmt.Print("set new sleep: 1700\n\n\n")
+	channel = strings.ToLower(args[1])
+	connect()
+	go readInput()
+	readChat()
+}
+
+func readInput() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		line = strings.Replace(line, "\n", "", -1)
+		line = strings.Replace(line, "\r", "", -1)
+		spl := strings.Split(line, " ")
+		switch len(spl) {
+		case 1:
+			if spl[0] == "" {
+				fmt.Println("usage:")
+				fmt.Println("\tmake a pyramid: Kappa 5")
+				fmt.Println("\tset new sleep: 1700")
+				break
+			}
+			slp, err := strconv.Atoi(spl[0])
+			if err != nil {
+				fmt.Println("error parsing sleep", err)
+				break
+			}
+			sleep = time.Duration(slp) * time.Millisecond
+			fmt.Println("sleep is now set to", sleep.String())
+		case 2:
+			count, err := strconv.Atoi(spl[1])
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			emote := spl[0]
+			go buildPyramid(count, emote)
+		}
+	}
+}
+
+func buildPyramid(count int, emote string) {
+	if !mod && sleep.Seconds() < 1.2 {
+		sleep = time.Millisecond * 1200
+		fmt.Println("set sleep to", sleep.String(), "because you are not mod")
+	}
+	emote += " "
+	for i := 1; i < count+1; i++ {
+		say(strings.Repeat(emote, i))
+		if sleep > 0 {
+			time.Sleep(sleep)
+		}
+	}
+	for i := count - 1; i > 0; i-- {
+		say(strings.Repeat(emote, i))
+		if sleep > 0 {
+			time.Sleep(sleep)
+		}
+	}
+}
